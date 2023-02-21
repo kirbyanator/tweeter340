152bab9bc4f02a148d93c57892db102235f1bfd4
kirbyanator <dp4ster007@me.com>
2023-02-21 14:30:28 -0700

    Services done

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/IsFollowerHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/IsFollowerHandler.java
@@ -2,7 +2,6 @@ package edu.byu.cs.tweeter.client.model.service.backgroundTask.handler;
 
 import android.os.Bundle;
 
-import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.IsFollowerTask;
 import edu.byu.cs.tweeter.client.model.service.observer.BooleanObserver;
 
